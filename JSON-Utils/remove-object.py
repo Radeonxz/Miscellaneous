ffd83f9653cf79c1aa280466dc355bbb4ac5bb1f
Radeonxz <xuanxxzhao@gmail.com>
2019-04-24 19:59:04 -0400

    ..

--- JSON-Utils/remove-object.py
+++ JSON-Utils/remove-object.py
@@ -2,13 +2,13 @@
 
 outputString = ""
 with open("inputSource.json", 'r') as jsonFile:
-    for l in jsonFile:
-        l = l[1:-2]
-        lArray = l.split(',')
-        lArray = lArray[1:]
-        l = ', '.join(lArray)
-        l = '{' + l + '}\n'
-        outputString += l
+  for l in jsonFile:
+    l = l[1:-2]
+    lArray = l.split(',')
+    lArray = lArray[1:]
+    l = ', '.join(lArray)
+    l = '{' + l + '}\n'
+    outputString += l
 
 ouputFile = open('output.json', 'w+')
 ouputFile.write(outputString)
