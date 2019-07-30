1834b6ff4e45aee2c317eca3d60a335f80035bef
Radeonxz <xuanxxzhao@gmail.com>
2019-07-30 17:19:46 -0400

    add pdf generator

---
+++ Scripts/GenInvoice-PDF/nodejs/index.js
@@ -0,0 +1,149 @@
+const { createInvoice } = require('./createInvoice.js');
+
+const invoice = {
+  shipping: {
+    client_id: 520,
+    name: 'John Doe',
+    address: '1234 Main Street',
+    city: 'San Francisco',
+    state: 'CA',
+    country: 'US',
+    postalcode: 94111
+  },
+  items: [
+    {
+      item: 'ACT-800',
+      description: 'Toner Cartridge',
+      quantity: 2,
+      amount: 6000
+    },
+    {
+      item: 'MAIL',
+      description: 'USB Cable Extender',
+      quantity: 1,
+      amount: 2000
+    },
+    {
+      item: 'LONGNAME',
+      description: 'Toner Cartridge',
+      quantity: 2,
+      amount: 6000
+    },
+    {
+      item: 'VERYLONG-NAME',
+      description: 'USB Cable Extender',
+      quantity: 1,
+      amount: 2000
+    },
+    {
+      item: 'VERYVERY-LONG-NAME',
+      description: 'Description can also be very long enough',
+      quantity: 2,
+      amount: 6000
+    },
+    {
+      item: '6',
+      description: 'USB Cable Extender',
+      quantity: 1,
+      amount: 2000
+    },
+    {
+      item: '7',
+      description: 'Toner Cartridge',
+      quantity: 2,
+      amount: 6000
+    },
+    {
+      item: '8',
+      description: 'USB Cable Extender',
+      quantity: 1,
+      amount: 2000
+    },
+    {
+      item: '9',
+      description: 'Toner Cartridge',
+      quantity: 2,
+      amount: 6000
+    },
+    {
+      item: '10',
+      description: 'USB Cable Extender',
+      quantity: 1,
+      amount: 2000
+    },
+    {
+      item: '11',
+      description: 'Toner Cartridge',
+      quantity: 2,
+      amount: 6000
+    },
+    {
+      item: '12',
+      description: 'USB Cable Extender',
+      quantity: 1,
+      amount: 2000
+    },
+    {
+      item: '13',
+      description: 'Toner Cartridge',
+      quantity: 2,
+      amount: 6000
+    },
+    {
+      item: '14',
+      description: 'USB Cable Extender',
+      quantity: 1,
+      amount: 2000
+    },
+    {
+      item: '15',
+      description: 'Toner Cartridge',
+      quantity: 2,
+      amount: 6000
+    },
+    {
+      item: '16',
+      description: 'USB Cable Extender',
+      quantity: 1,
+      amount: 2000
+    },
+    {
+      item: '17',
+      description: 'Toner Cartridge',
+      quantity: 2,
+      amount: 6000
+    },
+    {
+      item: '18',
+      description: 'USB Cable Extender',
+      quantity: 1,
+      amount: 2000
+    }
+  ],
+  subtotal: 0,
+  paid: 0,
+  invoice_nr: 131902
+};
+
+const contractor = {
+  user_id: 250,
+  company: 'Dumy Inc.',
+  address: '250 Dumy Street',
+  city: 'Dumy City',
+  province: 'DY',
+  country: 'DY',
+  postalcode: '2D5 U0M',
+  footer: 'Please pay your bill on time!'
+}
+const prefix = 'INV';
+const date = new Date();
+let month = date.getMonth() + 1;
+month = month < 10 ? `0${month}` : month;
+console.log('month', typeof month)
+const name = `${prefix}${invoice.invoice_nr}_${contractor.user_id}_${invoice.shipping.client_id}_AT_${date.getFullYear()}${month}${date.getDate()}.pdf`
+
+for(let i = 0; i < invoice.items.length; i++) {
+  invoice.subtotal += invoice.items[i].amount;
+}
+
+createInvoice(invoice, contractor, name);
